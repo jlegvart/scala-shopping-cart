5e2cf5a8080e222e900c16c3583ab885655c1611
jlegvart <jernej.legvart@protonmail.com>
2022-01-02 16:31:38 +0100

    flyway

--- src/main/scala/com/playground/shoppingcart/Server.scala
+++ src/main/scala/com/playground/shoppingcart/Server.scala
@@ -9,18 +9,25 @@ import scala.concurrent.ExecutionContext.global
 import io.circe.config.parser
 import com.playground.shoppingcart.config.ApplicationConfig
 import org.http4s.server
+import doobie.hikari.HikariTransactor
+import doobie.util.ExecutionContexts
+import com.playground.shoppingcart.config.DatabaseConfig
 
 object Server extends IOApp {
 
   def createServer[F[_]: Async]: Resource[F, server.Server] =
     for {
       config <- Resource.eval(ApplicationConfig.loadConfig[F]())
+      transactor <- DatabaseConfig.transactor(config.db)
+      _ <- Resource.eval(DatabaseConfig.initDB[F](config.db))
       server <-
         BlazeServerBuilder[F](global)
-          .bindHttp(8080, "localhost")
+          .bindHttp(config.server.port, config.server.host)
           .resource
     } yield server
 
-  def run(args: List[String]): IO[ExitCode] = createServer[IO].use(_ => IO.never).as(ExitCode.Success)
+  def run(
+    args: List[String]
+  ): IO[ExitCode] = createServer[IO].use(_ => IO.never).as(ExitCode.Success)
 
 }
--- src/main/scala/com/playground/shoppingcart/config/DatabaseConfig.scala
+++ src/main/scala/com/playground/shoppingcart/config/DatabaseConfig.scala
@@ -1,4 +1,42 @@
 package com.playground.shoppingcart.config
 
+import doobie.util.ExecutionContexts
+import doobie.hikari.HikariTransactor
+import cats.effect.kernel.Async
+import cats.effect.kernel.Resource
+import cats.effect.kernel.Sync
+import org.flywaydb.core.Flyway
+
 final case class Connections(poolSize: Int)
-final case class DatabaseConfig(url: String, user: String, password: String, driver: String, connections: Connections)
+
+final case class DatabaseConfig(
+  url: String,
+  user: String,
+  password: String,
+  driver: String,
+  connections: Connections,
+)
+
+object DatabaseConfig {
+
+  def transactor[F[_]: Async](dbConfig: DatabaseConfig): Resource[F, HikariTransactor[F]] =
+    for {
+      ce <- ExecutionContexts.fixedThreadPool[F](32)
+      xa <- HikariTransactor.newHikariTransactor[F](
+        dbConfig.driver,
+        dbConfig.url,
+        dbConfig.user,
+        dbConfig.password,
+        ce,
+      )
+    } yield xa
+
+  def initDB[F[_]: Sync](dbConfig: DatabaseConfig): F[Unit] = Sync[F].delay {
+    Flyway
+      .configure()
+      .dataSource(dbConfig.url, dbConfig.user, dbConfig.password)
+      .load()
+      .migrate()
+  }
+
+}
