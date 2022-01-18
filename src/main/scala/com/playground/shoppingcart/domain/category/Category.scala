80536dd30073357134e3571dfae0891cbdb57216
jlegvart <jernej.legvart@protonmail.com>
2022-01-18 12:27:47 +0100

    company, category classes added

---
+++ src/main/scala/com/playground/shoppingcart/domain/category/Category.scala
@@ -0,0 +1,3 @@
+package com.playground.shoppingcart.domain.category
+
+final case class Category(id: Option[Int], name: String)
---
+++ src/main/scala/com/playground/shoppingcart/domain/company/Company.scala
@@ -0,0 +1,3 @@
+package com.playground.shoppingcart.domain.company
+
+final case class Company(id: Option[Int], name: String)
