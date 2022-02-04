885e4f842817a7daaeb2b96b723bb4c882445224
jlegvart <jernej.legvart@protonmail.com>
2022-02-04 16:35:08 +0100

    error refactoring

--- src/main/scala/com/playground/shoppingcart/endpoint/AuthEndpoint.scala
+++ src/main/scala/com/playground/shoppingcart/endpoint/AuthEndpoint.scala
@@ -29,8 +29,12 @@ import tsec.passwordhashers.jca._
 
 import scala.concurrent.duration._
 
-class AuthEndpoint[F[_]: Async](userService: UserService[F], key: MacSigningKey[HMACSHA256])
-  extends Http4sDsl[F] {
+class AuthEndpoint[F[_]](
+  userService: UserService[F],
+  key: MacSigningKey[HMACSHA256],
+)(
+  implicit F: Async[F]
+) extends Http4sDsl[F] {
 
   implicit val userDecoder = jsonOf[F, RegisterRequest]
   implicit val logiDecoder = jsonOf[F, LoginRequest]
@@ -38,35 +42,45 @@ class AuthEndpoint[F[_]: Async](userService: UserService[F], key: MacSigningKey[
   private def loginEndpoint: HttpRoutes[F] = HttpRoutes.of[F] { case req @ POST -> Root / "login" =>
     val userVerification =
       for {
-        loginReq <- EitherT.liftF(req.as[LoginRequest])
-        user <- EitherT.fromOptionF(
-          userService.getUser(loginReq.username),
-          UserAuthenticationError(""),
-        )
-        check <- EitherT.liftF(
-          BCrypt.checkpw[F](loginReq.password.getBytes(), PasswordHash(user.password))
-        )
-        isVerified <-
-          if (check == Verified)
-            EitherT.rightT[F, UserAuthenticationError](user)
-          else
-            EitherT.leftT[F, User](UserAuthenticationError(""))
-      } yield isVerified
+        loginRequest <- req.as[LoginRequest]
+        user         <- userOrError(loginRequest.username)
+        verified     <- passwordVerified(loginRequest.password, user)
+      } yield user
 
-    userVerification.value.flatMap {
-      case Left(err) => Response(status = Status.Unauthorized).pure
-      case Right(user) =>
-        for {
-          claim <- JWTClaim(user.id.get, user.username, user.role.name).pure[F]
-          claims <- JWTClaims.withDuration[F](
-            customFields = Seq("user" -> claim.asJson),
-            expiration = Some(10.minutes),
-          )
-          jwt  <- JWTMac.buildToString[F, HMACSHA256](claims, key)
-          resp <- Ok(jwt)
-        } yield resp
-    }
+    userVerification
+      .flatMap(buildJWT)
+      .flatMap(Ok(_))
+      .handleErrorWith {
+        case UserAuthenticationError(_) => BadRequest("Invalid username or password")
+        case _: MessageFailure          => BadRequest()
+        case _                          => InternalServerError()
+      }
   }
+
+  private def userOrError(username: String): F[User] = userService.getUser(username).flatMap {
+    case Some(user) => F.pure(user)
+    case None       => F.raiseError(UserAuthenticationError("Invalid username"))
+  }
+
+  private def passwordVerified(passwordReq: String, user: User): F[Unit] =
+    for {
+      verification <- BCrypt.checkpw[F](passwordReq.getBytes(), PasswordHash(user.password))
+      verified <-
+        if (verification == Verified)
+          F.unit
+        else
+          F.raiseError(UserAuthenticationError("Invalid password"))
+    } yield verified
+
+  private def buildJWT(user: User): F[String] =
+    for {
+      claim <- JWTClaim(user.id.get, user.username, user.role.name).pure[F]
+      claims <- JWTClaims.withDuration[F](
+        customFields = Seq("user" -> claim.asJson),
+        expiration = Some(10.minutes),
+      )
+      jwt <- JWTMac.buildToString[F, HMACSHA256](claims, key)
+    } yield jwt
 
   private def registerEndpoint: HttpRoutes[F] = HttpRoutes.of[F] {
     case req @ POST -> Root / "register" =>
