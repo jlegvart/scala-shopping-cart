5ac237b30c04ff482db9a7800de2ffe2f7ba5028
jlegvart <jernej.legvart@protonmail.com>
2022-02-04 15:58:22 +0100

    auth error refactoring

--- src/main/scala/com/playground/shoppingcart/endpoint/validation/AuthValidation.scala
+++ src/main/scala/com/playground/shoppingcart/endpoint/validation/AuthValidation.scala
@@ -1,8 +1,6 @@
 package com.playground.shoppingcart.endpoint.validation
 
 import cats._
-import cats.data.EitherT
-import cats.data.OptionT
 import cats.effect.kernel.Async
 import cats.syntax.all._
 import com.playground.shoppingcart.domain.auth.JWTClaim
@@ -22,64 +20,57 @@ import tsec.jws.mac.JWTMac
 import tsec.mac.jca.HMACSHA256
 import tsec.mac.jca.MacSigningKey
 
-class AuthValidation[F[_]: Async](key: MacSigningKey[HMACSHA256]) {
+class AuthValidation[F[_]](key: MacSigningKey[HMACSHA256])(implicit F: Async[F]) {
 
   def asAuthUser(
     f: AuthorizedRequest[F] => F[Response[F]]
   )(
     request: Request[F]
-  ): F[Response[F]] = authUser(request).value.flatMap {
-    case Left(value) => Response[F](status = Status.Unauthorized).pure[F]
-    case Right(user) =>
-      val userId = OptionT
-        .fromOption(user.id)
-        .getOrElseF(Async[F].raiseError(new RuntimeException("User id is empty")))
+  ): F[Response[F]] = authUser(request)
+    .flatMap { user =>
+      f(AuthorizedRequest(request, new AuthorizedUser(user.id.get, user.username, user.role)))
+    }
+    .handleError {
+      case UserAuthenticationError(msg) => Response(status = Status.Unauthorized)
+      case _                            => Response(status = Status.InternalServerError)
+    }
 
-      userId.flatMap(id =>
-        f(AuthorizedRequest[F](request, new AuthorizedUser(id, user.username, user.role)))
-      )
-  }
-
-  private def authUser(request: Request[F]): EitherT[F, UserAuthenticationError, User] =
+  private def authUser(request: Request[F]): F[User] =
     for {
-      header <- EitherT.fromEither[F](
-        request
-          .headers
-          .get[Authorization]
-          .toRight(UserAuthenticationError("Invalid authorization header"))
-      )
-      tokenStr <- getToken(header.value).toRight(
-        UserAuthenticationError("Missing authorization token")
-      )
-      parsedToken <- EitherT(parseJWTToken(tokenStr, key))
-      user        <- EitherT.fromEither(userFromToken(parsedToken))
+      header      <- authHeader(request)
+      token       <- getToken(header.value)
+      parsedToken <- parseJWTToken(token, key)
+      user        <- userFromToken(parsedToken)
     } yield user
 
-  private def getToken(authHeader: String): OptionT[F, String] = {
+  private def authHeader(request: Request[F]): F[Authorization] =
+    request.headers.get[Authorization] match {
+      case Some(header) => F.pure(header)
+      case None         => F.raiseError(UserAuthenticationError("Missing auth header"))
+    }
+
+  private def getToken(authHeader: String): F[String] = {
     val authToken = authHeader.split(" ")
     if (authToken.length != 2)
-      OptionT.none
+      F.raiseError(UserAuthenticationError("Invalid token value"))
     else
-      OptionT.pure(authToken(1))
+      F.pure(authToken(1))
   }
 
   private def parseJWTToken(
     token: String,
     key: MacSigningKey[HMACSHA256],
-  ): F[Either[UserAuthenticationError, JWTMac[HMACSHA256]]] = JWTMac
+  ): F[JWTMac[HMACSHA256]] = JWTMac
     .verifyAndParse[F, HMACSHA256](token, key)
-    .flatMap(parsedToken => parsedToken.asRight[UserAuthenticationError].pure[F])
-    .handleError(_ => UserAuthenticationError("Invalid token").asLeft)
+    .handleErrorWith(_ => F.raiseError(UserAuthenticationError("Invalid token")))
 
-  private def userFromToken(token: JWTMac[HMACSHA256]): Either[UserAuthenticationError, User] =
-    token
-      .body
-      .getCustom[JWTClaim]("user")
-      .fold(
-        _ =>
-          UserAuthenticationError("Invalid JWT token structure, cannot extract user data")
-            .asLeft[User],
-        claims => User(Some(claims.userId), claims.username, "", Role.toRole(claims.role)).asRight,
-      )
+  private def userFromToken(
+    token: JWTMac[HMACSHA256]
+  ): F[User] = F.pure(token.body.getCustom[JWTClaim]("user")).flatMap {
+    case Left(failure) =>
+      F.raiseError(UserAuthenticationError("Invalid JWT token structure, cannot extract user data"))
+    case Right(claims) =>
+      F.pure(User(Some(claims.userId), claims.username, "", Role.toRole(claims.role)))
+  }
 
 }
